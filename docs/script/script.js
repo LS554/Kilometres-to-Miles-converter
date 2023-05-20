7c32297730ff4c5bc0b7466d8929fe351463516b
LS554 <london.sheard@outlook.com>
2023-05-20 15:23:18 -0400

    Updated conversion

--- docs/script/script.js
+++ docs/script/script.js
@@ -2,7 +2,7 @@ const input_box = document.getElementById('input_box');
 const output_box = document.getElementById('output_box');
 const KM_TO_M = 1.609344; // km to m
 
-function km_to_m() {
+function m_to_km() {
     const km = parseFloat(input_box.value);
     const m = km * KM_TO_M;
     if (input_box.value != '') {
@@ -12,7 +12,7 @@ function km_to_m() {
     }
 }
 
-function m_to_km() {
+function km_to_m() {
     const km = parseFloat(input_box.value);
     const m = km / KM_TO_M;
     if (input_box.value != '') {
